16b6644ac1a116ee3555b72cfa1842d1b9399017
agawa98 <grz-bog@wp.pl>
2022-05-30 17:01:29 +0200

    emoji textarea i cos tam posprzatalem

--- scripts/real.js
+++
@@ -1,7 +0,0 @@
-function zmienimg(){
-    document.getElementsByClassName("aber")[0].style.backgroundImage = "url('../media/img/players/arturo.jpg')";
-    document.getElementsByClassName("miko")[0].style.backgroundImage = "url('../media/img/players/miko.jpg')";
-    document.getElementsByClassName("grzbog")[0].style.backgroundImage = "url('../media/img/players/grz.jpg')";
-    document.getElementsByClassName("dkurek")[0].style.backgroundImage = "url('../media/img/players/damen.jpg')";
-    document.getElementsByClassName("frytka")[0].style.backgroundImage = "url('../media/img/players/jan.jpg')";
-}
