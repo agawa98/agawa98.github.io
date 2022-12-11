ff44383bd468be3e71afa09df2f84d9eaa27e360
agawa98 <62704143+agawa98@users.noreply.github.com>
2022-12-11 18:47:01 +0100

    goofymode upgrade

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,11 @@
 
 
 
+//goofy ahh mode
 
+var changeSpeed = 1000;
 
-function goofy(){
+function goofymaker(){
     var divs = document.getElementsByTagName("div")
     for(let i=0; i<divs.length; i++){
         let r=Math.floor(Math.random()*255)
@@ -18,13 +20,48 @@ function goofy(){
         let b=Math.floor(Math.random()*255)
         divs[i].style.backgroundColor = "rgb("+r+","+g+","+b+")"
         divs[i].style.color = "rgb("+(256-r)+","+(256-g)+","+(256-b)+")"
-    } 
+    }
 }
 
+function goofmanager(){
+    goofymaker()
+    goofchanger = setInterval(goofymaker,changeSpeed)
+}
+
+function speed(){
+    console.log(document.getElementById("goofyrange").value)
+    changeSpeed = document.getElementById("goofyrange").value
+    var divs = document.getElementsByTagName("div")
+    for(let i=0;i<divs.length;i++){
+        divs[i].style.transition = (changeSpeed/1000)+"s"
+    }
+    clearInterval(goofchanger)
+    goofchanger = setInterval(goofymaker,changeSpeed)
+}
+
+document.getElementById("goofer").addEventListener("click", ()=>{
+    let slider = document.getElementById("goofyrange")
+    let button = document.getElementById("goofer")
+
+    button.disabled = true
+    slider.style.display = "block"
+    slider.style.top = button.clientHeight+5+"px"
+    slider.style.right = 0;
+    slider.max = "2000"
+    console.log("asdad")
+    slider.min = "1"
+    slider.value = 1000;
+    slider.style.width = button.style.width
+    
+    goofmanager()
+})
 
 
 
+//warning
 
-document.getElementById("goofer").addEventListener("click", goofy)
+document.getElementById("warning").addEventListener("click", ()=>{
+    document.getElementById("warning").remove()
+})
 
 
